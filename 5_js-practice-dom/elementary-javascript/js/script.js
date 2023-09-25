474d6b269a450c03c76478864a153be5b954f614
KiYO <kiyoto_xyz@hotmail.com>
2023-09-25 17:58:10 +0900

    querySelecter関連 取得

---
+++ 5_js-practice-dom/elementary-javascript/js/script.js
@@ -0,0 +1,27 @@
+// id名で取得
+// console.log(document.querySelector("#js-title").outerHTML);
+// console.log(document.getElementById("js-title").outerHTML);
+
+// class名で取得
+// console.log(document.querySelector(".text").outerHTML);
+
+
+// const elements = document.querySelectorAll(".text");
+
+// for (let element of elements){
+//     console.log(element.outerHTML);
+// }
+
+// class名で取得
+// const elements = document.getElementsByClassName("text");
+// for (let element of elements){
+//     console.log(element.outerHTML);
+// }
+
+// // タグ目で取得
+// console.log(document.querySelector("h1").outerHTML);
+
+// // 属性で取得
+// console.log(document.querySelector('[href="https://www.daily-trial.com/"]').outerHTML);
+
+// document.querySelector("")
