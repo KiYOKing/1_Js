96a5ce0f3cc09f4a99c8876dd3ac9a8c116861c2
KiYO <kiyoto_xyz@hotmail.com>
2023-09-29 22:05:38 +0900

    jQuery トップへ戻るボタンを作ろう

--- 7_js-practice-jquery2/js/script.js
+++ 7_js-practice-jquery2/js/script.js
@@ -15,3 +15,8 @@ document.getElementById("js-button-alert").addEventListener("click",function(){
 jQuery(".js-accordion").on("click",function(){
     jQuery(this).next().slideToggle("slow");
 });
+
+// トップへ戻るボタンを作ろう
+jQuery("#js-button-top").on("click", function(){
+    jQuery("html,body").animate({scrollTop:0},1000);
+});
