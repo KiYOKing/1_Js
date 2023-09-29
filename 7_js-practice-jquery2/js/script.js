1d96bc0bc3c88c4ce1a146ca8f0a317f07e45ac9
KiYO <kiyoto_xyz@hotmail.com>
2023-09-29 20:41:42 +0900

    jQuery ボタンクリックアラート

---
+++ 7_js-practice-jquery2/js/script.js
@@ -0,0 +1,12 @@
+// ボタンクリックでアラートを表示
+// jQuery("#js-button-alert").on("click",function(){
+//     alert("おはよう！");
+// });
+
+// document.querySelector("#js-button-alert").addEventListener("click",function(){
+//     alert("おはよう！");
+// });
+
+document.getElementById("js-button-alert").addEventListener("click",function(){
+    alert("こんばんは！");
+});
