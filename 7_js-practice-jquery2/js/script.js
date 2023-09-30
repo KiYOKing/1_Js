fd8e6661904df316c529dcc38bd5aa37aeeb39d6
KiYO <kiyoto_xyz@hotmail.com>
2023-09-30 12:49:16 +0900

    jQuery フォーム入力値のアラート表示

--- 7_js-practice-jquery2/js/script.js
+++ 7_js-practice-jquery2/js/script.js
@@ -27,3 +27,29 @@ jQuery("#js-button-drawer").on("click",function(){
     jQuery("#js-drawer").slideToggle();
     jQuery("body").toggleClass("is-fixed");
 });
+
+// フォームに入力された値をアラート表示しよう
+// jQuery("#js-form-name").on("submit",function(){
+//     if(jQuery("#js-input-name").val() === ""){
+//         // alert("必須入力です。");
+//         jQuery(".error-message").text("必須入力です");
+//     }else{
+//         jQuery(".error-message").text("");
+//         alert(jQuery("#js-input-name").val() + "さん、こんにちは");
+//     };
+// });
+jQuery("#js-form-name").on("submit", function(e){
+    e.preventDefault();
+    const nameInput = jQuery("#js-input-name")
+    const yourName = nameInput.val();
+
+    if (!yourName){
+        nameInput.next(".error-message").text("必須入力です。");
+        return;
+    }
+
+    nameInput.next(".error-message").text("");
+    alert(`${yourName} さん、こんにちは`);
+
+    return false;
+})
